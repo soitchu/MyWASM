6af3d1144cdbd98ad2b0853f0b02768ceeff821d
soitchu <soitchu@users.noreply.github.com>
2024-04-22 15:51:59 -0700

    [skip ci] Async dealloc

--- Runtime/index.ts
+++ Runtime/index.ts
@@ -5,8 +5,8 @@ export async function init(filename: string, debug: boolean, wasmBuffer: Uint8Ar
     const wasmModuleBuffer = wasmBuffer === undefined ? fs.readFileSync(filename) : wasmBuffer;
 
     const memory = new WebAssembly.Memory({
-        initial: 24000,
-        maximum: 24000,
+        initial: 16800,
+        maximum: 16800,
     });
 
     const ini = "default" in WASM ? WASM.ini : WASM.ini;
